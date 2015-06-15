6a0db05cf6fdedcebb0501b0ae6e1b34d3102e87
Andre Dinh <dinh.andre@yahoo.com>
2015-06-15 15:42:59 -0700

    changed base,module,states,index.html,added interactions.js

---
+++ interactions.js
@@ -0,0 +1,9 @@
+$(window).scroll(function () {
+  if ( $(this).scrollTop() > 200 && !$('header').hasClass('open') ) {
+    $('header').addClass('open');
+    $('header').slideDown();
+   } else if ( $(this).scrollTop() <= 200 ) {
+    $('header').removeClass('open');
+    $('header').slideUp();
+  }
+});
